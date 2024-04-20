4c61e8dd915cd10512f5be95c48f6b2bbf64ae38
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-04-20 13:14:36 +0100

    made a tad better

--- extension/main.js
+++ extension/main.js
@@ -7,7 +7,6 @@ function sleep(ms) {
 // Function to perform actions when the page is loaded or changed
 async function getBwkCode() {
     console.log('Extinguished Spark -> Running getBwkCode()');
-    await sleep(2000);
     bwkHTML = document.getElementsByClassName("_Chip_bu06u_1")[0].innerHTML;
     bwkCode = bwkHTML.split(": ")[1];
     console.log('Extinguished Spark -> Got bookwork code', bwkCode);
@@ -15,12 +14,9 @@ async function getBwkCode() {
 
 
 // Listener for page load and change events
-document.addEventListener('DOMContentLoaded', getBwkCode);
+document.addEventListener('DOMContentLoaded', async function(){await sleep(2000); getBwkCode();});
 
 document.addEventListener('click', async function(event) {
-    var element = event.target;
-    if (element.tagName === 'A') {
-      console.log('Extinguished Spark -> <a> clicked, rescanning for bwk code');
+      console.log('Extinguished Spark -> clicked, rescanning for bwk code');
       getBwkCode();
-    } 
 });
