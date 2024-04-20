564a7cd11c6569b9f3d779cdc516ae9cd7ee65ca
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-04-20 14:19:21 +0100

    it can now get answers
    it also saves them :D
    you cannot load them to solve bwk yet, but hopefully i'll get that figured out soon

--- extension/main.js
+++ extension/main.js
@@ -1,10 +1,10 @@
 var bwkCode = "";
+var answersMap = {};
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Function to perform actions when the page is loaded or changed
 async function getBwkCode() {
     console.log('Extinguished Spark -> Running getBwkCode()');
     bwkHTML = document.getElementsByClassName("_Chip_bu06u_1")[0].innerHTML;
@@ -12,11 +12,35 @@ async function getBwkCode() {
     console.log('Extinguished Spark -> Got bookwork code', bwkCode);
 }
 
+async function getAnswers() {
+    let answers = [];
+    console.log('Extinguished Spark -> Continue clicked, getting answers');
+    answersFromInput = document.getElementsByTagName('input');
+    Array.prototype.forEach.call(answersFromInput, function(answer) {
+        answers.push(answer.value);
+    });
+    console.log('Extinguished Spark -> Got answers', answers);
+    answersMap[bwkCode] = answers;
+    console.log('Extinguished Spark -> Updated answersMap to', answersMap);
+    
+    // Update localStorage with the updated answersMap
+    localStorage.setItem('answersMap', JSON.stringify(answersMap));
+}
 
-// Listener for page load and change events
-document.addEventListener('DOMContentLoaded', async function(){await sleep(2000); getBwkCode();});
+document.addEventListener('DOMContentLoaded', async function(){
+    if (document.URL.toLowerCase().includes("sparx")){
+        await sleep(2000); getBwkCode();
+    } else {
+        console.log("Extinguished Spark -> Sparx not mentioned in the URL. You will now be left alone.")
+    }
+});
 
 document.addEventListener('click', async function(event) {
-      console.log('Extinguished Spark -> clicked, rescanning for bwk code');
-      getBwkCode();
+    if (document.URL.toLowerCase().includes("sparx")){
+        var element = event.target;
+        getBwkCode();
+        if (element.innerHTML === 'Continue' || element.innerHTML === 'Summary') {
+            getAnswers();
+        } 
+    }
 });
