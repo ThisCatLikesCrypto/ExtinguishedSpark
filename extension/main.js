d5d3f0984ead2a2d8501bc952680d808027ffa58
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-04-20 14:43:49 +0100

    added a WIP getSelectAnswers() and keydown
    some answers need to be selected, so I made a function that will start to handle (not done) that.
    
    Also made it so it detects on pressing enter as well
    
    and fun as well

--- extension/main.js
+++ extension/main.js
@@ -7,9 +7,13 @@ function sleep(ms) {
 
 async function getBwkCode() {
     console.log('Extinguished Spark -> Running getBwkCode()');
-    bwkHTML = document.getElementsByClassName("_Chip_bu06u_1")[0].innerHTML;
-    bwkCode = bwkHTML.split(": ")[1];
-    console.log('Extinguished Spark -> Got bookwork code', bwkCode);
+    try {
+        bwkHTML = document.getElementsByClassName("_Chip_bu06u_1")[0].innerHTML;
+        bwkCode = bwkHTML.split(": ")[1];
+        console.log('Extinguished Spark -> Got bookwork code', bwkCode);
+    } catch {
+        console.log("Extinguished Spark -> Getting bwk code failed. Assuming you're on the homepage or something.")
+    }
 }
 
 async function getAnswers() {
@@ -19,16 +23,22 @@ async function getAnswers() {
     Array.prototype.forEach.call(answersFromInput, function(answer) {
         answers.push(answer.value);
     });
+    //!Need to be able to detect whether it's a select answer or input answer and handle accordingly
     console.log('Extinguished Spark -> Got answers', answers);
     answersMap[bwkCode] = answers;
     console.log('Extinguished Spark -> Updated answersMap to', answersMap);
     
-    // Update localStorage with the updated answersMap
-    localStorage.setItem('answersMap', JSON.stringify(answersMap));
+}
+
+//!Being worked on
+async function getSelectAnswers() {
+    selected = document.getElementsByClassName('_OptionSelected_1q5vh_576')[0].innerHTML;
+    console.log(selected);
 }
 
 document.addEventListener('DOMContentLoaded', async function(){
     if (document.URL.toLowerCase().includes("sparx")){
+        console.log('%cExtinguished Spark Online.', 'font-weight: bold; font-size: 30px; color: aqua; text-shadow: 2px 2px 0 rgb(217,31,38)');
         await sleep(2000); getBwkCode();
     } else {
         console.log("Extinguished Spark -> Sparx not mentioned in the URL. You will now be left alone.")
@@ -41,6 +51,19 @@ document.addEventListener('click', async function(event) {
         getBwkCode();
         if (element.innerHTML === 'Continue' || element.innerHTML === 'Summary') {
             getAnswers();
+            getSelectAnswers();
         } 
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        if (document.URL.toLowerCase().includes("sparx")){
+            var element = event.target;
+            getBwkCode();
+            if (element.innerHTML === 'Continue' || element.innerHTML === 'Summary') {
+                getAnswers();
+            } 
+        }
     }
 });
